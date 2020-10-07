725bafb1ef111e7d4b9d0c9927c67a11605f64e3
Altman20 <70535952+Altman20@users.noreply.github.com>
2020-10-07 14:01:51 -0400

    up to date?

--- app/public/js/index.app.js
+++ app/public/js/index.app.js
@@ -2,6 +2,7 @@ var app = new Vue({
   el: '#triagePage',
   data: {
     ptList: [],
+    visitList: [],
     activePt: null,
     triageForm: {
       priority: null,
@@ -23,11 +24,30 @@ var app = new Vue({
         sexAtBirth: ""
       }
     },
+    dateSince(d) {
+      // Uses Luxon date API (see comment in index.html file)
+      return moment.utc(d).calendar();
+    },
+    age(d) {
+      return moment().diff(moment(d), 'years');
+    },
+    /**
+     * Given a priority, returns triage class
+     * or "" if not found
+     **/
+    priorityClass(p) {
+      const priorityClass = {
+        low: "triageMinor",
+        medium: "triageUrgent",
+        high: "triageCritical"
+      };
+
+      return p in priorityClass ? priorityClass[p] : "";
+    },
     handleNewPtForm( evt ) {
       // evt.preventDefault();  // Redundant w/ Vue's submit.prevent
 
       // TODO: Validate the data!
-
       fetch('api/records/post.php', {
         method:'POST',
         body: JSON.stringify(this.newPtForm),
@@ -41,18 +61,14 @@ var app = new Vue({
         // TODO: test a result was returned!
         this.ptList.push(json[0]);
       });
-
       console.log("Creating (POSTing)...!");
       console.log(this.newPtForm);
-
       this.newPtForm = this.newPtData();
     },
     handleTriageForm( evt ) {
       console.log("Form submitted!");
-
       this.triageForm.pt = this.activePt;
       console.log(this.triageForm);
-
     }
   },
   created() {
@@ -60,7 +76,12 @@ var app = new Vue({
     .then( response => response.json() )
     .then( json => {
       this.ptList = json;
-
+      console.log(json)}
+    );
+    fetch("api/visits/")
+    .then( response => response.json() )
+    .then( json => {
+      this.visitList = json;
       console.log(json)}
     );
     this.newPtForm = this.newPtData();
